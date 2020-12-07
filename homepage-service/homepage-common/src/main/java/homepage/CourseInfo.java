af3eb0a2643467c8cf7758a0047646ae72f25746
yingming006 <yingming006@foxmail>
2020-12-07 22:38:13 +0800

    add homepae-service

---
+++ homepage-service/homepage-common/src/main/java/homepage/CourseInfo.java
@@ -0,0 +1,24 @@
+package homepage;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CourseInfo {
+
+    private Long id;
+    private String courseName;
+    private String courseType;
+    private String courseIcon;
+    private String courseIntro;
+
+    public static CourseInfo invalid() {
+        return new CourseInfo(-1L, "", "", "", "");
+    }
+
+}
---
+++ homepage-service/homepage-common/src/main/java/homepage/CourseInfoRequest.java
@@ -0,0 +1,15 @@
+package homepage;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CourseInfoRequest {
+
+    private List<Long> ids;
+}
